ba1a43c0aa379d6f73d83f3f57d8a759b05c3c70
u22830368 <u22830368@tuks.co.za>
2025-11-25 15:58:32 +0200

    Add imports and database configuration to app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session,g
+import sqlite3
+import os
+from datetime import datetime, timedelta
+import random
+from werkzeug.security import generate_password_hash, check_password_hash
+
+project_root = os.path.dirname(os.path.abspath(__file__))#Ndumiso add
+DATABASE = os.path.join(project_root, 'inventory.db')
 
